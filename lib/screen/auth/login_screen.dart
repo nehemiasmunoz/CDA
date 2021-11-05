deeeb4af425995ec9758d4b1dc134c781dd637ca
Nehemias M <nehemiasmarvic.dev@gmail.com>
2021-11-05 12:07:54 -0300

    [UPD] login screen nueva apariencia login mas validator

--- lib/screen/auth/login_screen.dart
+++ lib/screen/auth/login_screen.dart
@@ -1,51 +1,48 @@
 import 'package:centro_actividades/screen/auth/components/rounded_container.dart';
 import 'package:centro_actividades/utils/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () {
+        //ocultar el teclado al tocar la pantalla
         FocusScope.of(context).requestFocus(new FocusNode());
       },
       child: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Login',
-                    // textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.02,
-                  ),
-                  RoundedContainer(child: createEmail()),
-                  RoundedContainer(child: createPassword()),
-                  RoundedContainer(
-                    color: kPrimaryColor,
-                    child: TextButton(
-                      child: Text(
-                        'Ingresar',
-                        style: TextStyle(color: Colors.white, fontSize: 16.0),
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 350.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/studyLogin.png'),
+                        ),
                       ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'assignments');
-                      },
                     ),
-                  ),
-                ],
+                    Text(
+                      'Login',
+                      style: kLabelStyle,
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    _CustomForm(),
+                  ],
+                ),
               ),
             ),
           ),
@@ -55,61 +52,96 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class createEmail extends StatelessWidget {
-  //const createEmail({Key? key}) : super(key: key);
+class _CustomForm extends StatefulWidget {
+  @override
+  State<_CustomForm> createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<_CustomForm> {
+  String emailValue = '';
+  int passwordValue = 0000;
+
+  bool passwordVisibility = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      cursorColor: kPrimaryColor,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        icon: Icon(
-          Icons.person,
-          color: kPrimaryColor,
-        ),
-        hintText: 'Email',
-      ),
-    );
-  }
-}
-
-class createPassword extends StatefulWidget {
-  // const createPassword({Key? key}) : super(key: key);
-
-  @override
-  _createPasswordState createState() => _createPasswordState();
-}
-
-class _createPasswordState extends State<createPassword> {
-  bool visibility = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      maxLines: 1,
-      obscureText: visibility,
-      keyboardType: TextInputType.number,
-      cursorColor: kPrimaryColor,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        icon: Icon(
-          Icons.lock,
-          color: kPrimaryColor,
-        ),
-        hintText: 'Contraseña',
-        suffixIcon: GestureDetector(
-          onTap: () {
-            setState(() {
-              visibility = !visibility;
-            });
-          },
-          child: Icon(
-            visibility == false ? Icons.visibility : Icons.visibility_off,
-            color: kPrimaryColor,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            onSaved: (value) {},
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !value.contains('@') ||
+                  value.length < 5) {
+                return 'Por favor ingresa un email valido';
+              }
+            },
+            cursorColor: kPrimaryColor,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'Email',
+              border: OutlineInputBorder(),
+            ),
           ),
-        ),
+          SizedBox(height: 10.0),
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty || value.length < 4) {
+                return 'Por favor completa este campo';
+              }
+            },
+            cursorColor: kPrimaryColor,
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            maxLines: 1,
+            minLines: 1,
+            maxLength: 4,
+            obscureText: passwordVisibility,
+            decoration: InputDecoration(
+              hintText: 'Contraseña',
+              border: OutlineInputBorder(),
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    passwordVisibility = !passwordVisibility;
+                  });
+                },
+                child: Icon(
+                  passwordVisibility == false
+                      ? Icons.visibility
+                      : Icons.visibility_off,
+                  color: kPrimaryColor,
+                ),
+              ),
+            ),
+          ),
+          RoundedContainer(
+            color: kPrimaryColor,
+            child: TextButton(
+              child: Text(
+                'Ingresar'.toUpperCase(),
+                style: kLabelStyle.copyWith(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                ),
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                  Navigator.pushNamed(context, 'assignments');
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 // Colores usados en la aplicacion
 
-// const kPrimaryColor = Color.fromRGBO(25, 118, 210, 1);
+const kPrimaryColor = Color.fromRGBO(25, 118, 210, 1);
 // const kPrimaryLightColor = Color.fromRGBO(186, 222, 252, 1);
-const kPrimaryColor = Color.fromRGBO(99, 173, 242, 1);
-const kPrimaryLightColor = Color.fromRGBO(167, 204, 237, 1);
+const kPrimaryLightColor = Color.fromRGBO(99, 173, 242, 1);
 const kBackgroundColor = Colors.white70;
+const kLabelStyle = TextStyle(
+  fontSize: 26.0,
+  fontWeight: FontWeight.bold,
+);
