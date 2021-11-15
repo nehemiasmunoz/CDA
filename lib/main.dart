b1573c515ed05c7b8de7f3c5142a4b4e2cb0077d
Nehemias M <nehemiasmarvic.dev@gmail.com>
2021-11-15 11:41:36 -0300

    [UPD] main añadido app state contiene los Providers
    [UPD] routes
    optimizacion de rutas
    
    [ADDED] screens
    optimizacion de exportaciones
    
    [UPD] assignments screen
    envio, recepcion y exposicion de datos
    
    [UPD] assignments model
    nuevos metodos y propiedades
    
    [DEL] assignments brain
    clase no necesaria
    
    [ADDED] loading screen
    pantalla de carga reutilizable
    
    [ADDED] assignments services
    archivo de conexion y proveedor de datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:centro_actividades/services/assignments_services.dart';
 import 'package:flutter/material.dart';
 import 'package:centro_actividades/routes/routes.dart';
 import 'package:centro_actividades/utils/constant.dart';
@@ -5,11 +6,26 @@ import 'package:centro_actividades/utils/constant.dart';
 //firebase connection
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AssignmentsServices())
+      ],
+      child: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
--- lib/screen/assignments/models/assigments_model.dart
+++ lib/screen/assignments/models/assigments_model.dart
@@ -1,7 +1,26 @@
-class AssignmentsModel {
-  String name;
-  String? imgUrl;
-  bool? active;
+// To parse this JSON data, do
+//
+//     final assignment = assignmentFromMap(jsonString);
 
-  AssignmentsModel({required this.name, this.imgUrl, this.active});
+import 'dart:convert';
+
+class AssignmentModel {
+  AssignmentModel({
+    required this.name,
+  });
+
+  String name;
+  String? id;
+  factory AssignmentModel.fromJson(String str) =>
+      AssignmentModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory AssignmentModel.fromMap(Map<String, dynamic> json) => AssignmentModel(
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "name": name,
+      };
 }
